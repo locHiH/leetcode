da8bee1a75944b6a7b647313c74805de1c33837a
hcol <282386589@qq.com>
2016-05-11 14:17:19 +0800

    keep going...

--- src/algorithms/question2/AC.java
+++ src/algorithms/question2/AC.java
@@ -1,5 +1,111 @@
 package algorithms.question2;
 
+import java.util.Random;
+
+/**
+ * 
+ * 因为是链表，遍历之前需要保存头结点：<br />
+ * ListNode n1 = l1; <br />
+ * ListNode n2 = l2; <br />
+ * 
+ * 两个链表相加，又不知道他们的节点的情况下，n1 的size 和n2 的size 不定长<br />
+ * 所以，循环的终止条件肯定是n1 == null && n2 == null的时候
+ * 
+ * 那么，在不循环的时候我们做了什么呢？ <br />
+ * 我们生成了第三条链表 <br />
+ * >>> h
+ * 
+ * 第三条链表的节点就是 new ListNode(sum % 10)
+ * 
+ * 在每次循环开始后的step 1 ： sum /= 10; 这句话的意义就在于进位标志每次都能参与到next的运算中来，叼得不行...
+ * 
+ * 然后在循环完了之后需要处理一种特殊情况：<br />
+ * n1.next == null && n2.next == null && (n1.val + n2.val) / 10 == 1的情况
+ * 在这种情况下，sum == 10了，但是next 都为空了，所以跳出循环，如果不做处理的话就会遗失掉一个node 所以，在最后: <br />
+ * 
+ * if (sum / 10 == 1) { temp.next = new ListNode(1); }
+ * 
+ * 如果用递归呢？！我需要用递归来尝试一下...虽然递归的开销大...但是这个算法的第三条node也是没必要的开销..so...lets do it!!!
+ * 
+ * @author qi
+ */
 public class AC {
 
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode n1 = l1;
+		ListNode n2 = l2;
+		ListNode h = new ListNode(0);
+		ListNode temp;
+		temp = h;
+		int sum = 0;
+		while (n1 != null || n2 != null) {
+
+			sum /= 10;
+
+			if (n1 != null) {
+				sum += n1.val;
+				n1 = n1.next;
+			}
+
+			if (n2 != null) {
+				sum += n2.val;
+				n2 = n2.next;
+			}
+
+			temp.next = new ListNode(sum % 10);
+			temp = temp.next;
+		}
+
+		if (sum / 10 == 1) {
+			temp.next = new ListNode(1);
+		}
+		return h.next;
+	}
+
+	/**
+	 * test cases
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ListNode l1 = new ListNode(0);
+		ListNode l2 = new ListNode(0);
+
+		Random mRandom = new Random();
+
+		ListNode t1 = l1;
+		ListNode t2 = l2;
+		for (int i = 0; i < 10; i++, t1 = t1.next) {
+			t1.next = new ListNode(mRandom.nextInt(9));
+
+			if (i == mRandom.nextInt(10))
+				continue;
+
+			t2.next = new ListNode(mRandom.nextInt(9));
+			t2 = t2.next;
+		}
+
+		System.out.println("l1:");
+		t1 = l1.next;
+		while (t1 != null) {
+			System.out.print(t1.val + " ");
+			t1 = t1.next;
+		}
+
+		System.out.println("\nl2:");
+
+		t2 = l2.next;
+		while (t2 != null) {
+			System.out.print(t2.val + " ");
+			t2 = t2.next;
+		}
+
+		System.out.println("\nresult:");
+
+		ListNode result = new Mine().addTwoNumbers(l2.next, l1.next);
+		while (result != null) {
+			System.out.print(result.val + " ");
+			result = result.next;
+		}
+	}
 }
--- src/algorithms/question2/Mine.java
+++ src/algorithms/question2/Mine.java
@@ -1,5 +1,120 @@
 package algorithms.question2;
 
+/**
+ * 
+ * @author qi
+ *
+ */
 public class Mine {
 
+	/**
+	 * 如何将进位优雅的，有效的传入到next的运算中去..
+	 * 
+	 * 如果我们只用两条node 且递归来做的话，第一件事就是需要判断这个node 的有效性
+	 * 
+	 * 原则上我们需要指出一条较长的list 来进行结果返回，但是，在题目的条件下，我们不进行遍历对比就没法知道l1,
+	 * l2哪条链表比较长，而且西安遍历一遍的话这不合理，所以...
+	 * 
+	 * @后记 运行居然比while 快了 1ms..为什么..但是这个实现有点丑..而且一点都不程序化(programmatically)
+	 * 
+	 *     有点可笑的是这个程序有点像合并链表(苦笑...WTF...
+	 * 
+	 * @param l1
+	 * @param l2
+	 * @return l1
+	 * 
+	 *         以 l1 头结点为节点返回整个运算结果
+	 * 
+	 */
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		// 递归终止条件
+		if (l1 == null && l2 == null) {
+			return null;
+		}
+		// 修正当前节点为10的情况
+		if (l1.equals(l2)) {
+			if (l1.val / 10 == 1) {
+				// 如果这个节点为尾节点的话，就需要新建一个节点
+				if (l1.next == null) {
+					l1.next = new ListNode(1);
+					l1.val %= 10;
+					return l1;
+				}
+				l1.next.val += 1;
+				l1.val = l1.val % 10;
+				l1.next = addTwoNumbers(l1.next, l1.next);
+			}
+			return l1;
+		}
+
+		// 嫁接判断
+		boolean needMove = (l1.next == null && l2 != null && l2.next != null);
+		// 运算当前节点
+		int sum = 0;
+		if (l1 != null) {
+			sum += l1.val;
+		}
+		if (l2 != null) {
+			sum += l2.val;
+		}
+		l1.val = sum % 10;
+		// 嫁接
+		if (needMove) {
+			l1.next = l2.next;
+		}
+		// 进位
+		if (sum / 10 == 1) {
+			// 如果这个节点为尾节点的话，就需要新建一个节点
+			if (l1.next == null) {
+				l1.next = new ListNode(1);
+				l1.val %= 10;
+				return l1;
+			}
+			l1.next.val += 1;
+		}
+		l1.next = addTwoNumbers(l1.next, l2 == null ? null : l2.next);
+		return l1;
+	}
+
+	/**
+	 * 
+	 * 第一次尝试，在看ac代码之前..
+	 * 
+	 * 进位标志无法进入下一个递归去...
+	 * 
+	 * @param l1
+	 * @param l2
+	 * @return
+	 */
+	// public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	//
+	// if (l1 == null && l2 == null) {
+	// return null;
+	// } else if (l1 != null && l2 == null) {
+	// if (l1.val >= 10) {
+	// l1.val %= 10;
+	// }
+	// l1.next = addTwoNumbers(l1.next, new ListNode(1));
+	// return l1;
+	// } else if (l1 == null && l2 != null) {
+	// l1 = new ListNode(0);
+	// l1.next = null;
+	// }
+	//
+	// boolean overRange = false;
+	// if (l1.val + l2.val >= 10)
+	// overRange = true;
+	//
+	// l1.val = (l1.val + l2.val) % 10;
+	//
+	// if (overRange) {
+	// if (l1.next == null)
+	// l1.next = new ListNode(1);
+	// else
+	// l1.next.val += 1;
+	// }
+	//
+	// l1.next = addTwoNumbers(l1.next, l2.next);
+	// return l1;
+	// }
 }
